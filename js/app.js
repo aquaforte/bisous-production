3efe7cd24c238f956d15057f4e8d8139d4bfbf6a
aquaforte <cyril@aqua-forte.net>
2015-01-04 23:46:08 +0100

    Ajout d'une animation de préload.

--- js/app.js
+++ js/app.js
@@ -31,8 +31,91 @@ var homeItv = null,
   }
   
   
-
   
+	var support = { animations : Modernizr.cssanimations },
+		container = document.getElementById("ip-container"),
+		header = container.querySelector( 'header.ip-header' ),
+		loader = new PathLoader( document.getElementById( 'ip-loader-circle' ) ),
+		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
+  
+
+  function init() {
+		var onEndInitialAnimation = function() {
+    		
+    		_console("onEndInitialAnimation()");
+			if( support.animations ) {
+				this.removeEventListener( animEndEventName, onEndInitialAnimation );
+			}
+
+			startLoading();
+		};
+
+		// disable scrolling
+		window.addEventListener( 'scroll', noscroll );
+
+		// initial animation
+		$( '#ip-container' ).addClass( 'loading' );
+
+		if( support.animations ) {
+			container.addEventListener( animEndEventName, onEndInitialAnimation );
+		}
+		else {
+			onEndInitialAnimation();
+		}
+	}
+
+	function startLoading() {
+		// simulate loading something..
+		var simulationFn = function(instance) {
+			var progress = 0,
+				interval = setInterval( function() {
+					progress = Math.min( progress + Math.random() * 0.1, 1 );
+
+					instance.setProgress( progress );
+
+					// reached the end
+					if( progress === 1 ) {
+    					
+						$( '#ip-container' ).removeClass( 'loading' );
+						$( '#ip-container' ).addClass( 'loaded' );
+						clearInterval( interval );
+
+						var onEndHeaderAnimation = function(ev) {
+    						
+                            _console("onEndHeaderAnimation()");
+                            $.fn.fullpage.moveTo('home');
+							
+							if( support.animations ) {
+								if( ev.target !== header ) return;
+								this.removeEventListener( animEndEventName, onEndHeaderAnimation );
+							}
+
+						//	$('body').addClass( 'layout-switch' );
+							window.removeEventListener( 'scroll', noscroll );
+
+						};
+
+						if( support.animations ) {
+							header.addEventListener( animEndEventName, onEndHeaderAnimation );
+						}
+						else {
+							onEndHeaderAnimation();
+						}
+						
+					}
+				}, 80 );
+		};
+
+		loader.setProgressFn( simulationFn );
+	}
+	
+	function noscroll() {
+		window.scrollTo( 0, 0 );
+	}
+	
+	init();
 
   $(document).ready(function() {
     
